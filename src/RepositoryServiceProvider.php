3d016fa564585726c05fbbd1746669fbfb53eeda
Sam Taylor <sam@taylornetwork.ca>
2016-11-06 11:00:27 -0500

    Added config

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -2,10 +2,36 @@
 
 namespace TaylorNetwork\Repository;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
+    private $directory;
+
+    private $dirPath;
+
+    private $namespace;
+
+    private $naming;
+
+    private $drivers;
+
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+
+        $this->directory = config('repository.directory', 'Repositories');
+        
+        $this->dirPath = app_path() . '/' . $this->directory;
+        
+        $this->namespace = 'App\\'.str_replace('{directory}',$this->directory,config('repository.namespace','{directory}')).'\\';
+        
+        $this->naming = config('repository.naming', [ 'interface' => '{name}Repository', 'class' => '{driver}{name}' ]);
+        
+        $this->drivers = config('repository.drivers', [ 'Eloquent' ]);
+    }
+
     /**
      * Bootstrap the application services.
      *
@@ -13,7 +39,9 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/config/repository.php' => config_path('repository.php'),
+        ]);
     }
 
     /**
@@ -23,17 +51,84 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $dir = app_path() . '/Repositories';
+        $this->mergeConfigFrom(
+            __DIR__.'/config/repository.php', 'repository'
+        );
 
-        foreach(scandir($dir) as $repository)
+        //$this->commands([ ]); @todo Actually make the commands work
+
+        $this->registerRepositories();
+
+    }
+
+    /**
+     * Replace namespace
+     *
+     * @param string $name
+     * @return string
+     */
+    public function replaceNamespace($name)
+    {
+        return str_replace('{name}', $name, $this->namespace);
+    }
+
+    /**
+     * Get the interface name
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getInterface($name)
+    {
+        return $this->replaceNamespace($name) . str_replace('{name}', $name, $this->naming['interface']);
+    }
+
+    /**
+     * Get the class name
+     *
+     * @param string $name
+     * @param string $path
+     * @return string
+     */
+    public function getClass($name, $path)
+    {
+        $class = str_replace('{name}', $name, $this->naming['class']);
+        $files = [];
+
+        foreach(glob($path . '/*.php') as $file)
         {
-            if(substr($repository, 0, 1) != '.')
+            $files[] = substr(last(explode('/', $file)), 0, -4);
+        }
+
+        foreach($this->drivers as $driver)
+        {
+            $replaceDriver = str_replace('{driver}', $driver, $class);
+            if(in_array($replaceDriver, $files))
             {
-                $this->app->singleton(
-                    "App\\Repositories\\{$repository}\\{$repository}Repository",
-                    "App\\Repositories\\{$repository}\\Eloquent{$repository}"
-                );
+                $class = $replaceDriver;
+                break;
             }
+        }
+
+        return $this->replaceNamespace($name).$class;
+    }
+
+    /**
+     * Register the repositories
+     */
+    public function registerRepositories()
+    {
+        if(preg_match('/{name}/', $this->namespace))
+        {
+            foreach(glob($this->dirPath . '/*') as $repositoryName)
+            {
+                $path = $this->dirPath . '/' . $repositoryName;
+                $this->app->singleton($this->getInterface($repositoryName), $this->getClass($repositoryName, $path));
+            }
+        }
+        else
+        {
+            // @todo figure this out
         }
     }
 }
---
+++ src/config/repository.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Base Directory
+     |--------------------------------------------------------------------------
+     |
+     | This is where your repositories will be stored.
+     | 
+     | 'App\' will be prepended, '\' will be appended.
+     | 
+     */
+    'directory' => 'Repositories',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Repository Namespace Pattern
+     |--------------------------------------------------------------------------
+     |
+     | Use this to make additional folders in which to keep each repository.
+     | {directory} will be replaced by value above 
+     | {name} will be replaced by a repository name 
+     |
+     | Examples: 
+     |      User repository in App\Repositories\User\UserRepository
+     |      Use '{directory}\\{name}' 
+     | 
+     |      User repository in App\Repositories\UserRepository
+     |      Use '{directory}'
+     |
+     |      User repository in App\Repositories\RepositoryForUser\UserRepository
+     |      Use '{directory}\\RepositoryFor{name}'
+     |      
+     */
+    'namespace' => '{directory}\\{name}',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Naming Convention
+     |--------------------------------------------------------------------------
+     |
+     | How you intend on naming your repositories.
+     | {name} is replaced by repository name.
+     | {driver} is replaced by a driver in the driver variable below.
+     |
+     */
+    'naming' => [
+        'interface' => '{name}Repository',
+        'class' => '{driver}{name}',
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Drivers
+     |--------------------------------------------------------------------------
+     |
+     | The drivers that you will use in order of search. Service provider will 
+     | look for the first value and if one is found using the naming convention
+     | above it will add that as a singleton. 
+     |
+     */
+    'drivers' => [
+        'Eloquent',
+        'Storage',
+    ],
+];
